73c634ee076e0a1c372433aa71e592c70a0758dd
sarahrosecooper <sarahrosecooperx@gmail.com>
2021-02-21 20:45:25 -0500

    Created axiosWithAuth.js

--- src/helpers/axiosWithAuth.js
+++ src/helpers/axiosWithAuth.js
@@ -1,4 +1,14 @@
 import axios from "axios";
 
+const axiosWithAuth = () => {
+  const token = localStorage.getItem("token");
+  return axios.create({
+    baseURL: "http://localhost:5000/api",
+    headers: {
+      Authorization: token,
+    },
+  });
+};
+
 //Task List:
-//Build and export a function used to send in our authorization token
+// 👍 Build and export a function used to send in our authorization token
